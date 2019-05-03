f47e953275e3699cca08a66fe72fd7336f9fd7dd
Roman Chigvintsev <rchigvintsev@gmail.com>
2019-05-03 14:01:31 +0300

    Add a little more tests for 'Strings.isBlank' method

--- src/app/util/strings.spec.ts
+++ src/app/util/strings.spec.ts
@@ -4,4 +4,32 @@ describe('Strings', () => {
   it(`'isBlank' should return 'true' for 'null'`, () => {
     expect(Strings.isBlank(null)).toBeTruthy();
   });
+
+  it(`'isBlank' should return 'true' for 'undefined'`, () => {
+    expect(Strings.isBlank(undefined)).toBeTruthy();
+  });
+
+  it(`'isBlank' should return 'true' for empty string`, () => {
+    expect(Strings.isBlank('')).toBeTruthy();
+  });
+
+  it(`'isBlank' should return 'true' for ' '`, () => {
+    expect(Strings.isBlank(' ')).toBeTruthy();
+  });
+
+  it(`'isBlank' should return 'false' for 'abc'`, () => {
+    expect(Strings.isBlank('abc')).toBeFalsy();
+  });
+
+  it(`'isBlank' should return 'false' for ' abc'`, () => {
+    expect(Strings.isBlank(' abc')).toBeFalsy();
+  });
+
+  it(`'isBlank' should return 'false' for 'abc '`, () => {
+    expect(Strings.isBlank('abc ')).toBeFalsy();
+  });
+
+  it(`'isBlank' should return 'false' for ' abc '`, () => {
+    expect(Strings.isBlank(' abc ')).toBeFalsy();
+  });
 });
