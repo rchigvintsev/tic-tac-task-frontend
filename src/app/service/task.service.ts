74fc8f6081f2fdea7a3b441b2873bde5a5ee0b5c
Roman Chigvintsev <rchigvintsev@gmail.com>
2019-05-04 17:27:15 +0300

    Load only uncompleted tasks

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
@@ -21,7 +21,8 @@ export class TaskService {
   }
 
   getTasks(): Observable<Task[]> {
-    return this.http.get<any>(this.taskUrl).pipe(
+    const params = new HttpParams().set('completed', 'false');
+    return this.http.get<any>(`${this.taskUrl}/search/findByCompleted`, {params: params}).pipe(
       map(response => {
         const tasks = [];
         for (let json of response._embedded.tasks)
