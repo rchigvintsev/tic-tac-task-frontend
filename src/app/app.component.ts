08256c43769b68cbbdf6984b37bff84d7c3f95e6
Roman Chigvintsev <rchigvintsev@gmail.com>
2019-06-03 13:12:51 +0300

    Change moment.js locale when application language is changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import {Component} from '@angular/core';
 
+import {LangChangeEvent, TranslateService} from '@ngx-translate/core';
+
+import * as moment from 'moment';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,10 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'Orchestra';
+
+  constructor(private translate: TranslateService) {
+    this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      moment.locale(event.lang);
+    });
+  }
 }
