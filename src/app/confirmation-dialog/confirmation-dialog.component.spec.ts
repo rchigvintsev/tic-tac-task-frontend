7160a3ae97d1932eb25edbf3a1c4f71f4469bd0d
Roman Chigvintsev <rchigvintsev@gmail.com>
2019-07-04 12:12:08 +0300

    Add tests for ConfirmationDialogComponent

--- src/app/confirmation-dialog/confirmation-dialog.component.spec.ts
+++ src/app/confirmation-dialog/confirmation-dialog.component.spec.ts
@@ -3,11 +3,18 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {HttpClient} from '@angular/common/http';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {By} from '@angular/platform-browser';
 
 import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 
 import {TranslateHttpLoaderFactory} from '../app.module';
 import {ConfirmationDialogComponent} from './confirmation-dialog.component';
+
+class MatDialogRefMock {
+  // noinspection JSUnusedGlobalSymbols
+  close() {
+  }
+}
 
 describe('ConfirmationDialogComponent', () => {
   let component: ConfirmationDialogComponent;
@@ -28,7 +35,7 @@ describe('ConfirmationDialogComponent', () => {
       declarations: [ConfirmationDialogComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
       providers: [
-        {provide: MatDialogRef, useValue: {}},
+        {provide: MatDialogRef, useClass: MatDialogRefMock},
         {provide: MAT_DIALOG_DATA, useValue: []}
       ]
     }).compileComponents();
@@ -42,5 +49,27 @@ describe('ConfirmationDialogComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should return true when "Yes" button was clicked', () => {
+    const matDialogRef = fixture.debugElement.injector.get(MatDialogRef);
+    spyOn(matDialogRef, 'close');
+    const yesButton = fixture.debugElement.query(By.css('#yes_button'));
+    fixture.whenStable().then(() => {
+      yesButton.nativeElement.click();
+      fixture.detectChanges();
+      expect(matDialogRef.close).toHaveBeenCalledWith(true);
+    });
+  });
+
+  it('should return false when "No" button was clicked', () => {
+    const matDialogRef = fixture.debugElement.injector.get(MatDialogRef);
+    spyOn(matDialogRef, 'close');
+    const noButton = fixture.debugElement.query(By.css('#no_button'));
+    fixture.whenStable().then(() => {
+      noButton.nativeElement.click();
+      fixture.detectChanges();
+      expect(matDialogRef.close).toHaveBeenCalledWith(false);
+    });
   });
 });
