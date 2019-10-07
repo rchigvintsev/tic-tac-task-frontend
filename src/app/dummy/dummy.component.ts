e724a4c2a5e115a82dfc024f9c6a6651fb30c803
Roman Chigvintsev <rchigvintsev@gmail.com>
2019-10-07 14:08:33 +0300

    Add dummy component that does nothing

---
+++ src/app/dummy/dummy.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-dummy',
+  templateUrl: './dummy.component.html',
+  styleUrls: ['./dummy.component.styl']
+})
+export class DummyComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}
