9c620fa865e2fdfce6f52796379e6ea3a582b8e8
Roman Chigvintsev <rchigvintsev@gmail.com>
2019-10-26 21:45:17 +0300

    Add 'Sign in with Facebook' button

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,6 +6,7 @@ import {MatIconRegistry} from '@angular/material/icon';
 import {TranslateService} from '@ngx-translate/core';
 
 import {AbstractComponent} from '../abstract-component';
+import {ConfigService} from '../service/config.service';
 
 @Component({
   selector: 'app-login',
@@ -13,9 +14,23 @@ import {AbstractComponent} from '../abstract-component';
   styleUrls: ['./login.component.styl']
 })
 export class LoginComponent extends AbstractComponent {
-  constructor(router: Router, translate: TranslateService, iconRegistry: MatIconRegistry, domSanitizer: DomSanitizer) {
+  config: ConfigService;
+
+  constructor(
+    router: Router,
+    translate: TranslateService,
+    config: ConfigService,
+    iconRegistry: MatIconRegistry,
+    domSanitizer: DomSanitizer
+  ) {
     super(router, translate);
+    this.config = config;
     iconRegistry.addSvgIcon('logo-google',
       domSanitizer.bypassSecurityTrustResourceUrl('../assets/img/btn_google_light_normal.svg'));
+  }
+
+  buildAuthorizationUri(provider: string): string {
+    const redirectUri = `${this.config.selfBaseUrl}/${this.translate.currentLang}/login/callback`;
+    return `${this.config.apiBaseUrl}/oauth2/authorization/${provider}?client-redirect-uri=${redirectUri}`;
   }
 }
