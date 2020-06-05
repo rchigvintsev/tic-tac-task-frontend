08ccf26196829efac43b05b47fcf57d3c983fdc8
Roman Chigvintsev <rchigvintsev@gmail.com>
2020-06-05 13:24:24 +0300

    Make day as a minimal unit of localized relative date

--- src/app/pipe/localized-relative-date.pipe.spec.ts
+++ src/app/pipe/localized-relative-date.pipe.spec.ts
@@ -1,15 +1,32 @@
 import {getTestBed, TestBed} from '@angular/core/testing';
+import {HttpClient} from '@angular/common/http';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 import * as moment from 'moment';
 
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+
 import {LocalizedRelativeDatePipe} from './localized-relative-date.pipe';
+import {TranslateHttpLoaderFactory} from '../app.module';
 
 describe('LocalizedRelativeDate', () => {
   let injector: TestBed;
   let datePipe: LocalizedRelativeDatePipe;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({providers: [LocalizedRelativeDatePipe]});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: TranslateHttpLoaderFactory,
+            deps: [HttpClient]
+          }
+        }),
+      ],
+      providers: [LocalizedRelativeDatePipe]
+    });
     injector = getTestBed();
     datePipe = injector.get(LocalizedRelativeDatePipe);
 
@@ -27,5 +44,17 @@ describe('LocalizedRelativeDate', () => {
 
   it('should return localized relative date', () => {
     expect(datePipe.transform(moment().add(1, 'month').toDate())).toBe('in a month');
+  });
+
+  it('should return localized relative date for today', () => {
+    expect(datePipe.transform(moment().toDate())).toBe('today');
+  });
+
+  it('should return localized relative date for yesterday', () => {
+    expect(datePipe.transform(moment().subtract(1, 'day').toDate())).toBe('yesterday');
+  });
+
+  it('should return localized relative date for tomorrow', () => {
+    expect(datePipe.transform(moment().add(1, 'day').toDate())).toBe('tomorrow');
   });
 });
--- src/app/pipe/localized-relative-date.pipe.ts
+++ src/app/pipe/localized-relative-date.pipe.ts
@@ -2,15 +2,41 @@ import {Pipe, PipeTransform} from '@angular/core';
 
 import * as moment from 'moment';
 
+import {TranslateService} from '@ngx-translate/core';
+
 @Pipe({
   name: 'localizedRelativeDate',
   pure: false
 })
 export class LocalizedRelativeDatePipe implements PipeTransform {
+  constructor(private translate: TranslateService) {
+  }
+
   transform(value: any, ...args: any[]): any {
     if (value == null) {
       return value;
     }
-    return moment(value, moment.HTML5_FMT.DATETIME_LOCAL_MS).fromNow();
+
+    const momentValue = moment(value);
+
+    const startOfDay = moment().startOf('day');
+    const endOfDay = moment().endOf('day');
+    if (momentValue.isSameOrAfter(startOfDay) && momentValue.isSameOrBefore(endOfDay)) {
+      return this.translate.instant('today');
+    }
+
+    if (momentValue.isBefore(moment())) {
+      const yesterday = moment().subtract(1, 'day').startOf('day');
+      if (momentValue.isSameOrAfter(yesterday)) {
+        return this.translate.instant('yesterday');
+      }
+    } else {
+      const tomorrow = moment().add(1, 'day').endOf('day');
+      if (momentValue.isSameOrBefore(tomorrow)) {
+        return this.translate.instant('tomorrow');
+      }
+    }
+
+    return momentValue.fromNow();
   }
 }
