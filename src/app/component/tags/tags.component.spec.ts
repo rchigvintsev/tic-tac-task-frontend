ce83a8d34e124c9d6df5c7cbf9543313c5cbf22b
Roman Chigvintsev <rchigvintsev@gmail.com>
2020-12-13 13:33:45 +0300

    Remove tag menu from tag list

--- src/app/component/tags/tags.component.spec.ts
+++ src/app/component/tags/tags.component.spec.ts
@@ -1,6 +1,5 @@
 import {async, ComponentFixture, getTestBed, TestBed} from '@angular/core/testing';
 import {NavigationEnd, Router, RouterEvent} from '@angular/router';
-import {MatDialog} from '@angular/material/dialog';
 import {By} from '@angular/platform-browser';
 
 import {of, Subject} from 'rxjs';
@@ -10,14 +9,6 @@ import {TestSupport} from '../../test/test-support';
 import {ConfigService} from '../../service/config.service';
 import {TagService} from '../../service/tag.service';
 import {Tag} from '../../model/tag';
-
-class MatDialogMock {
-  open() {
-    return {
-      afterClosed: () => of(true)
-    };
-  }
-}
 
 describe('TagsComponent', () => {
   let component: TagsComponent;
@@ -30,10 +21,7 @@ describe('TagsComponent', () => {
     TestBed.configureTestingModule({
       imports: TestSupport.IMPORTS,
       declarations: TestSupport.DECLARATIONS,
-      providers: [
-        {provide: MatDialog, useClass: MatDialogMock},
-        {provide: ConfigService, useValue: {apiBaseUrl: 'http://backend.com'}}
-      ]
+      providers: [{provide: ConfigService, useValue: {apiBaseUrl: 'http://backend.com'}}]
     }).compileComponents();
   }));
 
@@ -47,9 +35,6 @@ describe('TagsComponent', () => {
       tags.push(new Tag().deserialize({id: i + 1, name: `Test tag ${i + 1}`}));
     }
     spyOn(tagService, 'getTags').and.returnValue(of(tags));
-    spyOn(tagService, 'createTag').and.callFake(t => of(t));
-    spyOn(tagService, 'updateTag').and.callFake(t => of(t));
-    spyOn(tagService, 'deleteTag').and.returnValue(of(null));
 
     createdTagSource = new Subject<Tag>();
     spyOn(tagService, 'getCreatedTag').and.returnValue(createdTagSource.asObservable());
@@ -64,128 +49,6 @@ describe('TagsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  it('should show tag menu trigger button on tag list item mouse over', () => {
-    const tagId = component.tags[0].id;
-    const menuTriggerColumnSelector = By.css('.tag-' + tagId + ' .menu-trigger-column');
-    fixture.whenStable().then(() => {
-      component.onTagListItemMouseOver(component.tags[0]);
-      fixture.detectChanges();
-      expect(fixture.debugElement.query(menuTriggerColumnSelector).nativeElement.getAttribute('hidden')).toBeNull();
-    });
-  });
-
-  it('should hide tag menu trigger button on tag list item mouse out', () => {
-    component.onTagListItemMouseOver(component.tags[0]);
-    const tagId = component.tags[0].id;
-    const menuTriggerColumnSelector = By.css('.tag-' + tagId + ' .menu-trigger-column');
-    fixture.whenStable().then(() => {
-      component.onTagListItemMouseOut();
-      fixture.detectChanges();
-      expect(fixture.debugElement.query(menuTriggerColumnSelector).nativeElement.getAttribute('hidden')).toEqual('');
-    });
-  });
-
-  it('should not hide tag menu trigger button on tag list item mouse out when tag menu is opened', () => {
-    component.onTagListItemMouseOver(component.tags[0]);
-    const tagId = component.tags[0].id;
-    const menuTriggerColumnSelector = By.css('.tag-' + tagId + ' .menu-trigger-column');
-    fixture.whenStable().then(() => {
-      component.onTagMenuOpened();
-      component.onTagListItemMouseOut();
-      fixture.detectChanges();
-      expect(fixture.debugElement.query(menuTriggerColumnSelector).nativeElement.getAttribute('hidden')).toBeNull();
-    });
-  });
-
-  it('should hide tag menu trigger button on tag menu closed', () => {
-    component.onTagListItemMouseOver(component.tags[0]);
-    const tagId = component.tags[0].id;
-    const menuTriggerColumnSelector = By.css('.tag-' + tagId + ' .menu-trigger-column');
-    fixture.whenStable().then(() => {
-      component.onTagMenuClosed();
-      fixture.detectChanges();
-      expect(fixture.debugElement.query(menuTriggerColumnSelector).nativeElement.getAttribute('hidden')).toEqual('');
-    });
-  });
-
-  it('should show tag form on edit tag button click', () => {
-    const tag = component.tags[0];
-    fixture.whenStable().then(() => {
-      component.onEditTagButtonClick(tag);
-      fixture.detectChanges();
-      const tagForm = fixture.debugElement.query(By.css(`#tag_list .tag-${tag.id} .tag-form`));
-      expect(tagForm).toBeTruthy();
-    });
-  });
-
-  it('should hide tag form on "cancelTagButton" click', () => {
-    const tag = component.tags[0];
-    fixture.whenStable().then(() => {
-      component.tagFormModel = tag.clone();
-      component.onCancelTagEditButtonClick();
-      fixture.detectChanges();
-      const tagForm = fixture.debugElement.query(By.css(`#tag_list .tag-${tag.id} .tag-form`));
-      expect(tagForm).toBeFalsy();
-    });
-  });
-
-  it('should hide tag name column on edit tag button click', () => {
-    const tag = component.tags[0];
-    fixture.whenStable().then(() => {
-      component.onEditTagButtonClick(tag);
-      fixture.detectChanges();
-      const tagNameColumn = fixture.debugElement.query(By.css(`#tag_list .tag-${tag.id} .name-column`));
-      expect(tagNameColumn).toBeFalsy();
-    });
-  });
-
-  it('should save tag on "saveTagButton" click', () => {
-    fixture.whenStable().then(() => {
-      component.tagFormModel = component.tags[0].clone();
-      component.tagFormModel.name = 'New tag';
-      component.onSaveTagButtonClick();
-      fixture.detectChanges();
-      expect(tagService.updateTag).toHaveBeenCalled();
-    });
-  });
-
-  it('should change tag color on color change complete', () => {
-    fixture.whenStable().then(() => {
-      component.tagFormModel = component.tags[0].clone();
-      const colorEvent = {color: {hex: '#0000ff'}} as any;
-      component.onColorChangeComplete(colorEvent);
-      expect(component.tagFormModel.color).toEqual(colorEvent.color.hex);
-    });
-  });
-
-  it('should not save tag with blank name on "saveTagButton" click', () => {
-    fixture.whenStable().then(() => {
-      component.tagFormModel = component.tags[0].clone();
-      component.tagFormModel.name = ' ';
-      component.onSaveTagButtonClick();
-      fixture.detectChanges();
-      expect(tagService.updateTag).not.toHaveBeenCalled();
-    });
-  });
-
-  it('should throw error on "saveTagButton" click when tag model is not found by id', () => {
-    fixture.whenStable().then(() => {
-      component.tagFormModel = component.tags[0].clone();
-      component.tagFormModel.id = -1;
-      expect(() => component.onSaveTagButtonClick()).toThrow(new Error('Tag is not found by id -1'));
-    });
-  });
-
-  it('should delete tag', () => {
-    fixture.whenStable().then(() => {
-      const tagToDelete = component.tags[0];
-      component.onDeleteTagButtonClick(tagToDelete);
-      fixture.detectChanges();
-      expect(component.tags.length).toBe(2);
-      expect(component.tags[0]).not.toEqual(tagToDelete);
-    });
   });
 
   it('should highlight selected menu item', () => {
--- src/app/component/tags/tags.component.ts
+++ src/app/component/tags/tags.component.ts
@@ -1,20 +1,16 @@
-import {AfterViewChecked, ChangeDetectorRef, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {NavigationEnd, Router, RouterEvent} from '@angular/router';
-import {MatDialog} from '@angular/material/dialog';
 
 import {Subject} from 'rxjs';
 import {takeUntil} from 'rxjs/operators';
 
 import {TranslateService} from '@ngx-translate/core';
-import {ColorEvent} from 'ngx-color';
 
 import {Tag} from '../../model/tag';
 import {TagService} from '../../service/tag.service';
 import {AuthenticationService} from '../../service/authentication.service';
 import {LogService} from '../../service/log.service';
 import {WebServiceBasedComponent} from '../web-service-based.component';
-import {ConfirmationDialogComponent} from '../confirmation-dialog/confirmation-dialog.component';
-import {Strings} from '../../util/strings';
 import {PathMatcher} from '../../util/path-matcher';
 
 @Component({
@@ -22,21 +18,9 @@ import {PathMatcher} from '../../util/path-matcher';
   templateUrl: './tags.component.html',
   styleUrls: ['./tags.component.styl']
 })
-export class TagsComponent extends WebServiceBasedComponent implements OnInit, OnDestroy, AfterViewChecked {
-  readonly TAG_COLORS = [
-    '#e57373', '#f06292', '#ba68c8', '#9575cd', '#7986cb',
-    '#64b5f6', '#4dd0e1', '#4db6ac', '#81c784', '#dce775',
-    '#fff176', '#ffb74d', '#a1887f', '#e0e0e0', '#90a4ae'
-  ];
-
+export class TagsComponent extends WebServiceBasedComponent implements OnInit, OnDestroy {
   tags: Tag[];
-  tagFormModel: Tag;
-  tagMenuOpened: boolean;
-  tagNameInputFocused: boolean;
 
-  @ViewChild('tagNameInput')
-  private tagNameInput: ElementRef;
-  private selectedTag: Tag;
   private pathMatcher: PathMatcher;
   private componentDestroyed = new Subject<boolean>();
 
@@ -44,9 +28,7 @@ export class TagsComponent extends WebServiceBasedComponent implements OnInit, O
               translate: TranslateService,
               authenticationService: AuthenticationService,
               log: LogService,
-              private tagService: TagService,
-              private dialog: MatDialog,
-              private changeDetector: ChangeDetectorRef) {
+              private tagService: TagService) {
     super(translate, router, authenticationService, log);
   }
 
@@ -68,80 +50,8 @@ export class TagsComponent extends WebServiceBasedComponent implements OnInit, O
     this.componentDestroyed.complete();
   }
 
-  ngAfterViewChecked(): void {
-    if (this.tagFormModel && !this.tagNameInputFocused) {
-      this.tagNameInput.nativeElement.focus();
-      this.tagNameInputFocused = true;
-      // It is required to prevent ExpressionChangedAfterItHasBeenCheckedError
-      this.changeDetector.detectChanges();
-    }
-  }
-
-  isTagSelected(tag: Tag): boolean {
-    return this.selectedTag && this.selectedTag.id === tag.id;
-  }
-
-  isTagBeingEdited(tag: Tag): boolean {
-    return this.tagFormModel && this.tagFormModel.id === tag.id;
-  }
-
-  isTagFormValid(): boolean {
-    return this.tagFormModel && !Strings.isBlank(this.tagFormModel.name);
-  }
-
   isRouterLinkActive(path: string): boolean {
     return this.pathMatcher && this.pathMatcher.matches(path);
-  }
-
-  onTagListItemMouseOver(tag: Tag) {
-    this.selectedTag = tag;
-  }
-
-  onTagListItemMouseOut() {
-    if (!this.tagMenuOpened) {
-      this.selectedTag = null;
-    }
-  }
-
-  onTagMenuOpened() {
-    this.tagMenuOpened = true;
-  }
-
-  onTagMenuClosed() {
-    this.tagMenuOpened = false;
-    this.selectedTag = null;
-  }
-
-  onEditTagButtonClick(tag: Tag) {
-    this.tagFormModel = tag.clone();
-    this.tagNameInputFocused = false;
-  }
-
-  onDeleteTagButtonClick(tag: Tag) {
-    const title = this.translate.instant('attention');
-    const content = this.translate.instant('delete_tag_question');
-    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
-      width: '400px',
-      restoreFocus: false,
-      data: {title, content}
-    });
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) {
-        this.deleteTag(tag);
-      }
-    });
-  }
-
-  onSaveTagButtonClick() {
-    this.saveTag();
-  }
-
-  onCancelTagEditButtonClick() {
-    this.tagFormModel = null;
-  }
-
-  onColorChangeComplete(e: ColorEvent): void {
-    this.tagFormModel.color = e.color.hex;
   }
 
   private onNavigationEnd(e: NavigationEnd) {
@@ -150,28 +60,5 @@ export class TagsComponent extends WebServiceBasedComponent implements OnInit, O
 
   private onTagCreate(tag: Tag) {
     return this.tags.push(tag);
-  }
-
-  private saveTag() {
-    if (this.tagFormModel && !Strings.isBlank(this.tagFormModel.name)) {
-      const tagIndex = this.tags.findIndex(t => t.id === this.tagFormModel.id);
-      if (tagIndex < 0) {
-        throw new Error(`Tag is not found by id ${this.tagFormModel.id}`);
-      }
-      const tag = this.tags[tagIndex];
-      if (!this.tagFormModel.equals(tag)) {
-        this.tagService.updateTag(this.tagFormModel).subscribe(savedTag => {
-          this.tagFormModel = null;
-          this.tags[tagIndex] = savedTag;
-        }, this.onServiceCallError.bind(this));
-      } else {
-        this.tagFormModel = null;
-      }
-    }
-  }
-
-  private deleteTag(tag: Tag) {
-    this.tagService.deleteTag(tag).subscribe(_ => this.tags = this.tags.filter(t => t.id !== tag.id),
-      this.onServiceCallError.bind(this));
   }
 }
