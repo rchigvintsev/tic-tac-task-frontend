5e86aa34eb4d297a87e10b6c64a8e894537d5f47
Roman Chigvintsev <rchigvintsev@gmail.com>
2020-12-15 14:20:42 +0300

    Add tests for 'ColorPickerDialogComponent'

--- src/app/component/color-picker-dialog/color-picker-dialog.component.spec.ts
+++ src/app/component/color-picker-dialog/color-picker-dialog.component.spec.ts
@@ -2,6 +2,8 @@ import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 
+import {ColorEvent} from 'ngx-color';
+
 import {ColorPickerDialogComponent} from './color-picker-dialog.component';
 import {TestSupport} from '../../test/test-support';
 
@@ -35,5 +37,25 @@ describe('ColorPickerDialogComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should save selected color on color change complete', () => {
+    const colorEvent = {color: {hex: '#00ff00'}} as any;
+    component.onColorChangeComplete(colorEvent as ColorEvent);
+    expect(component.color).toEqual(colorEvent.color.hex);
+  });
+
+  it('should close dialog with positive result on "OK" button click', () => {
+    const matDialogRef = fixture.debugElement.injector.get(MatDialogRef);
+    spyOn(matDialogRef, 'close');
+    component.onOkButtonClick();
+    expect(matDialogRef.close).toHaveBeenCalledWith({result: true, color: component.color});
+  });
+
+  it('should close dialog with negative result "Cancel" button click', () => {
+    const matDialogRef = fixture.debugElement.injector.get(MatDialogRef);
+    spyOn(matDialogRef, 'close');
+    component.onCancelButtonClick();
+    expect(matDialogRef.close).toHaveBeenCalledWith({result: false});
   });
 });
