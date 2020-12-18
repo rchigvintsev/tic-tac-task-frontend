cfb5041970d4caadc09933c6ae4113b6884946a4
Roman Chigvintsev <rchigvintsev@gmail.com>
2020-12-18 13:37:13 +0300

    Remove task list menu from sidenav

--- src/app/component/fragment/task-lists/task-lists.component.spec.ts
+++ src/app/component/fragment/task-lists/task-lists.component.spec.ts
@@ -1,6 +1,5 @@
 import {async, ComponentFixture, getTestBed, TestBed} from '@angular/core/testing';
 import {NavigationEnd, Router, RouterEvent} from '@angular/router';
-import {MatDialog} from '@angular/material/dialog';
 import {By} from '@angular/platform-browser';
 
 import {of, Subject} from 'rxjs';
@@ -10,14 +9,6 @@ import {TestSupport} from '../../../test/test-support';
 import {ConfigService} from '../../../service/config.service';
 import {TaskListService} from '../../../service/task-list.service';
 import {TaskList} from '../../../model/task-list';
-
-class MatDialogMock {
-  open() {
-    return {
-      afterClosed: () => of({result: true})
-    };
-  }
-}
 
 describe('TaskListsComponent', () => {
   let component: TaskListsComponent;
@@ -29,10 +20,7 @@ describe('TaskListsComponent', () => {
     TestBed.configureTestingModule({
       imports: TestSupport.IMPORTS,
       declarations: TestSupport.DECLARATIONS,
-      providers: [
-        {provide: MatDialog, useClass: MatDialogMock},
-        {provide: ConfigService, useValue: {apiBaseUrl: 'http://backend.com'}}
-      ]
+      providers: [{provide: ConfigService, useValue: {apiBaseUrl: 'http://backend.com'}}]
     }).compileComponents();
   }));
 
@@ -53,7 +41,6 @@ describe('TaskListsComponent', () => {
       }
       return of(result);
     });
-    spyOn(taskListService, 'deleteTaskList').and.returnValue(of(null));
 
     routerEvents = new Subject();
     const router = injector.get(Router);
@@ -65,60 +52,6 @@ describe('TaskListsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  it('should show task list menu trigger button on task list list item mouse over', () => {
-    const taskListId = component.taskLists[0].id;
-    const menuTriggerColumnSelector = By.css('.task-list-' + taskListId + ' .menu-trigger-column');
-    fixture.whenStable().then(() => {
-      component.onTaskListListItemMouseOver(component.taskLists[0]);
-      fixture.detectChanges();
-      expect(fixture.debugElement.query(menuTriggerColumnSelector).nativeElement.getAttribute('hidden')).toBeNull();
-    });
-  });
-
-  it('should hide task list menu trigger button on task list list item mouse out', () => {
-    component.onTaskListListItemMouseOver(component.taskLists[0]);
-    const taskListId = component.taskLists[0].id;
-    const menuTriggerColumnSelector = By.css('.task-list-' + taskListId + ' .menu-trigger-column');
-    fixture.whenStable().then(() => {
-      component.onTaskListListItemMouseOut();
-      fixture.detectChanges();
-      expect(fixture.debugElement.query(menuTriggerColumnSelector).nativeElement.getAttribute('hidden')).toEqual('');
-    });
-  });
-
-  it('should not hide task list menu trigger button on task list list item mouse out when task list menu is opened', () => {
-    component.onTaskListListItemMouseOver(component.taskLists[0]);
-    const taskListId = component.taskLists[0].id;
-    const menuTriggerColumnSelector = By.css('.task-list-' + taskListId + ' .menu-trigger-column');
-    fixture.whenStable().then(() => {
-      component.onTaskListMenuOpened();
-      component.onTaskListListItemMouseOut();
-      fixture.detectChanges();
-      expect(fixture.debugElement.query(menuTriggerColumnSelector).nativeElement.getAttribute('hidden')).toBeNull();
-    });
-  });
-
-  it('should hide task list menu trigger button on task list menu closed', () => {
-    component.onTaskListListItemMouseOver(component.taskLists[0]);
-    const taskListId = component.taskLists[0].id;
-    const menuTriggerColumnSelector = By.css('.task-list-' + taskListId + ' .menu-trigger-column');
-    fixture.whenStable().then(() => {
-      component.onTaskListMenuClosed();
-      fixture.detectChanges();
-      expect(fixture.debugElement.query(menuTriggerColumnSelector).nativeElement.getAttribute('hidden')).toEqual('');
-    });
-  });
-
-  it('should delete task list', () => {
-    fixture.whenStable().then(() => {
-      const taskListToDelete = component.taskLists[0];
-      component.onDeleteTaskListButtonClick(taskListToDelete);
-      fixture.detectChanges();
-      expect(component.taskLists.length).toBe(2);
-      expect(component.taskLists[0]).not.toEqual(taskListToDelete);
-    });
   });
 
   it('should highlight selected menu item', () => {
--- src/app/component/fragment/task-lists/task-lists.component.ts
+++ src/app/component/fragment/task-lists/task-lists.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {NavigationEnd, Router, RouterEvent} from '@angular/router';
 import {NgForm} from '@angular/forms';
-import {MatDialog} from '@angular/material/dialog';
 
 import {TranslateService} from '@ngx-translate/core';
 
@@ -10,7 +9,6 @@ import {TaskListService} from '../../../service/task-list.service';
 import {AuthenticationService} from '../../../service/authentication.service';
 import {LogService} from '../../../service/log.service';
 import {WebServiceBasedComponent} from '../../web-service-based.component';
-import {ConfirmationDialogComponent} from '../confirmation-dialog/confirmation-dialog.component';
 import {PathMatcher} from '../../../util/path-matcher';
 import {Strings} from '../../../util/strings';
 
@@ -26,17 +24,14 @@ export class TaskListsComponent extends WebServiceBasedComponent implements OnIn
   taskListFormSubmitEnabled = false;
 
   taskLists: TaskList[];
-  taskListMenuOpened: boolean;
 
-  private selectedTaskList: TaskList;
   private pathMatcher: PathMatcher;
 
   constructor(router: Router,
               translate: TranslateService,
               authenticationService: AuthenticationService,
               log: LogService,
-              private taskListService: TaskListService,
-              private dialog: MatDialog) {
+              private taskListService: TaskListService) {
     super(translate, router, authenticationService, log);
   }
 
@@ -51,10 +46,6 @@ export class TaskListsComponent extends WebServiceBasedComponent implements OnIn
     });
   }
 
-  isTaskListSelected(taskList: TaskList): boolean {
-    return this.selectedTaskList && this.selectedTaskList.id === taskList.id;
-  }
-
   isRouterLinkActive(path: string): boolean {
     return this.pathMatcher && this.pathMatcher.matches(path);
   }
@@ -65,40 +56,6 @@ export class TaskListsComponent extends WebServiceBasedComponent implements OnIn
 
   onTaskListFormSubmit() {
     this.createTaskList(this.taskListFormModel);
-  }
-
-  onTaskListListItemMouseOver(taskList: TaskList) {
-    this.selectedTaskList = taskList;
-  }
-
-  onTaskListListItemMouseOut() {
-    if (!this.taskListMenuOpened) {
-      this.selectedTaskList = null;
-    }
-  }
-
-  onTaskListMenuOpened() {
-    this.taskListMenuOpened = true;
-  }
-
-  onTaskListMenuClosed() {
-    this.taskListMenuOpened = false;
-    this.selectedTaskList = null;
-  }
-
-  onDeleteTaskListButtonClick(taskList: TaskList) {
-    const title = this.translate.instant('attention');
-    const content = this.translate.instant('delete_task_list_question');
-    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
-      width: '400px',
-      restoreFocus: false,
-      data: {title, content}
-    });
-    dialogRef.afterClosed().subscribe(result => {
-      if (result.result) {
-        this.deleteTaskList(taskList);
-      }
-    });
   }
 
   private onNavigationEnd(e: NavigationEnd) {
@@ -112,11 +69,5 @@ export class TaskListsComponent extends WebServiceBasedComponent implements OnIn
         this.taskListForm.resetForm();
       }, this.onServiceCallError.bind(this));
     }
-  }
-
-  private deleteTaskList(taskList: TaskList) {
-    this.taskListService.deleteTaskList(taskList).subscribe(_ => {
-      this.taskLists = this.taskLists.filter(tl => tl.id !== taskList.id);
-    }, this.onServiceCallError.bind(this));
   }
 }
