1a446dad0dad80fa2ef2851b9ea5d4fe234bc729
Roman Chigvintsev <rchigvintsev@gmail.com>
2021-02-07 23:58:59 +0300

    Implement passwords matching validator

---
+++ src/app/validator/passwords-match.directive.spec.ts
@@ -0,0 +1,61 @@
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {Component, ViewChild} from '@angular/core';
+import {FormsModule, NgForm} from '@angular/forms';
+import {By} from '@angular/platform-browser';
+
+import {PasswordsMatchValidatorDirective} from './passwords-match.directive';
+
+@Component({
+  template: `
+    <form #testForm="ngForm" passwordsMatch>
+      <input name="password" type="password" ngModel>
+      <input name="password-repeat" type="password" ngModel>
+    </form>
+  `
+})
+class TestComponent {
+  @ViewChild('testForm')
+  testForm: NgForm;
+}
+
+describe('PasswordsMatchValidatorDirective', () => {
+  let fixture: ComponentFixture<TestComponent>;
+  let component: TestComponent;
+
+  beforeEach(async(() => TestBed.configureTestingModule({
+    imports: [FormsModule],
+    declarations: [TestComponent, PasswordsMatchValidatorDirective]
+  })));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestComponent);
+    fixture.detectChanges();
+    component = fixture.componentInstance;
+  });
+
+  it('should mark form as invalid when passwords do not match', () => {
+    const testForm = component.testForm;
+    const passwordInput = fixture.debugElement.query(By.css('[name=password]'));
+    const passwordRepeatInput = fixture.debugElement.query(By.css('[name=password-repeat]'));
+
+    fixture.whenStable().then(() => {
+      passwordInput.nativeElement.value = '12345';
+      passwordInput.nativeElement.dispatchEvent(new Event('input'));
+
+      passwordRepeatInput.nativeElement.value = '54321';
+      passwordRepeatInput.nativeElement.dispatchEvent(new Event('input'));
+
+      expect(testForm.control.valid).toBeFalsy();
+      expect(testForm.control.hasError('passwordsMatch', ['password'])).toBeTruthy();
+      expect(testForm.control.hasError('passwordsMatch', ['password-repeat'])).toBeTruthy();
+
+      const passwordInputControl = testForm.control.get('password');
+      expect(passwordInputControl.valid).toBeFalsy();
+      expect(passwordInputControl.hasError('passwordsMatch')).toBeTruthy();
+
+      const passwordRepeatInputControl = testForm.control.get('password-repeat');
+      expect(passwordRepeatInputControl.valid).toBeFalsy();
+      expect(passwordRepeatInputControl.hasError('passwordsMatch')).toBeTruthy();
+    });
+  });
+});
--- src/app/validator/passwords-match.directive.ts
+++ src/app/validator/passwords-match.directive.ts
@@ -1,8 +1,36 @@
 import {Directive} from '@angular/core';
-import {FormGroup, NG_VALIDATORS, ValidationErrors, Validator, ValidatorFn} from '@angular/forms';
+import {AbstractControl, FormGroup, NG_VALIDATORS, ValidationErrors, Validator, ValidatorFn} from '@angular/forms';
+
+import {Strings} from '../util/strings';
+
+function isControlDirty(control: AbstractControl): boolean {
+  return control && control.dirty;
+}
 
 function passwordsMatchValidator(): ValidatorFn {
   return (form: FormGroup): ValidationErrors => {
+    const passwordControl = form.controls.password;
+    const passwordRepeatControl = form.controls['password-repeat'];
+
+    if (isControlDirty(passwordControl) && isControlDirty(passwordRepeatControl)) {
+      const password = passwordControl.value;
+      const repeatedPassword = passwordRepeatControl.value;
+      if (password !== repeatedPassword) {
+        passwordControl.markAsTouched();
+        passwordControl.setErrors({passwordsMatch: true});
+        passwordRepeatControl.markAsTouched();
+        passwordRepeatControl.setErrors({passwordsMatch: true});
+      } else {
+        passwordControl.setErrors({passwordsMatch: false});
+        passwordRepeatControl.setErrors({passwordsMatch: false});
+        if (!Strings.isBlank(password)) {
+          passwordControl.markAsUntouched();
+        }
+        if (!Strings.isBlank(repeatedPassword)) {
+          passwordRepeatControl.markAsUntouched();
+        }
+      }
+    }
     return null;
   };
 }
