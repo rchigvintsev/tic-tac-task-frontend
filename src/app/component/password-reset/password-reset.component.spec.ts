0ceb7e884c744bf65563986c3e688a1de0d59ad9
Roman Chigvintsev <rchigvintsev@gmail.com>
2021-03-21 11:15:53 +0300

    Start password reset procedure on form submit

--- src/app/component/password-reset/password-reset.component.spec.ts
+++ src/app/component/password-reset/password-reset.component.spec.ts
@@ -1,11 +1,22 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, getTestBed, TestBed} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
+
+import {of} from 'rxjs';
 
 import {TestSupport} from '../../test/test-support';
 import {PasswordResetComponent} from './password-reset.component';
+import {AlertService} from '../../service/alert.service';
+import {I18nService} from '../../service/i18n.service';
+import {UserService} from '../../service/user.service';
+import {ConfigService} from '../../service/config.service';
+import {Config} from '../../model/config';
 
 describe('PasswordResetComponent', () => {
   let component: PasswordResetComponent;
   let fixture: ComponentFixture<PasswordResetComponent>;
+  let i18nService: I18nService;
+  let alertService: AlertService;
+  let userService: UserService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -15,6 +26,19 @@ describe('PasswordResetComponent', () => {
   }));
 
   beforeEach(() => {
+    const injector = getTestBed();
+
+    const configService = injector.get(ConfigService);
+    configService.setConfig(new Config());
+
+    i18nService = injector.get(I18nService);
+
+    alertService = injector.get(AlertService);
+    spyOn(alertService, 'info').and.stub();
+
+    userService = injector.get(UserService);
+    spyOn(userService, 'resetPassword').and.callFake(_ => of(true));
+
     fixture = TestBed.createComponent(PasswordResetComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -23,4 +47,46 @@ describe('PasswordResetComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should reset password on password reset form submit', () => {
+    fixture.whenStable().then(() => {
+      // For some reason two-way binding does not work in tests when input is placed within form
+      component.email = 'alice@mail.com';
+      setInputValue('email_input', component.email);
+      fixture.detectChanges();
+
+      component.onPasswordResetFormSubmit();
+      expect(userService.resetPassword).toHaveBeenCalled();
+    });
+  });
+
+  it('should not reset password when email is empty', () => {
+    fixture.whenStable().then(() => {
+      // For some reason two-way binding does not work in tests when input is placed within form
+      component.email = '';
+      setInputValue('email_input', component.email);
+      fixture.detectChanges();
+
+      component.onPasswordResetFormSubmit();
+      expect(userService.resetPassword).not.toHaveBeenCalled();
+    });
+  });
+
+  it('should show info message on password reset', () => {
+    fixture.whenStable().then(() => {
+      // For some reason two-way binding does not work in tests when input is placed within form
+      component.email = 'alice@mail.com';
+      setInputValue('email_input', component.email);
+      fixture.detectChanges();
+
+      component.onPasswordResetFormSubmit();
+      expect(alertService.info).toHaveBeenCalledWith(i18nService.translate('password_reset_confirmation_link_sent'));
+    });
+  });
+
+  function setInputValue(inputId: string, value: string) {
+    const emailInput = fixture.debugElement.query(By.css('#' + inputId)).nativeElement;
+    emailInput.value = value;
+    emailInput.dispatchEvent(new Event('input'));
+  }
 });
--- src/app/component/password-reset/password-reset.component.ts
+++ src/app/component/password-reset/password-reset.component.ts
@@ -1,20 +1,46 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {Router} from '@angular/router';
+
+import {WebServiceBasedComponent} from '../web-service-based.component';
+import {I18nService} from '../../service/i18n.service';
+import {AuthenticationService} from '../../service/authentication.service';
+import {LogService} from '../../service/log.service';
+import {UserService} from '../../service/user.service';
+import {AlertService} from '../../service/alert.service';
 
 @Component({
   selector: 'app-password-reset',
   templateUrl: './password-reset.component.html',
   styleUrls: ['./password-reset.component.styl']
 })
-export class PasswordResetComponent implements OnInit {
+export class PasswordResetComponent extends WebServiceBasedComponent implements OnInit {
+  @ViewChild('passwordResetForm', {read: NgForm})
+  passwordResetForm: NgForm;
+
   email: string;
 
-  constructor() {
+  constructor(i18nService: I18nService,
+              authenticationService: AuthenticationService,
+              log: LogService,
+              router: Router,
+              private userService: UserService,
+              private alertService: AlertService) {
+    super(i18nService, authenticationService, log, router);
   }
 
   ngOnInit() {
   }
 
   onPasswordResetFormSubmit() {
+    if (this.passwordResetForm.valid) {
+      this.userService.resetPassword(this.email)
+        .subscribe(_ => this.onPasswordReset(), error => this.onServiceCallError(error));
+    }
+  }
 
+  private onPasswordReset() {
+    this.alertService.info(this.i18nService.translate('password_reset_confirmation_link_sent', {email: this.email}));
+    this.passwordResetForm.resetForm();
   }
 }
